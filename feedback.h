ba19144c23f619552cde63a8a3038dc09a500cad
Krishna Raj <62538406+krishnaraj2912@users.noreply.github.com>
2023-10-08 14:13:04 +0530

    Update feedback.h

--- feedback.h
+++ feedback.h
@@ -3,18 +3,22 @@
 #include <string.h>
 #pragma once
 class feedback
-{   char name[20],type;//student or teacher or mess worker
-    char feedback_words[100];
+{   
+    char type;
+    std:string name;
+    std:string feedback_words[100];
 public:
-    void feedback_enter(char n[20],char t)
-    {   strcpy(name,n);
+    void feedback_enter(std:string n,char t)
+    {   
+        strcpy(name,n);
         type=t;
         cout<<"\n Enter your complaints / regards towards the  mess ";
         cin.ignore();
         gets(feedback_words);
     }
     void print_feedback()
-    {   cout<<"\n The feedback is from : "<<name;
+    {   
+        cout<<"\n The feedback is from : "<<name;
         cout<<"\n The feedback is : ";
         cout<<feedback_words;
     }
