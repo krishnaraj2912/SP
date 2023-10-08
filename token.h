070a6d98f7877f20c28d2d7c155202ac5b47139c
Krishna Raj <62538406+krishnaraj2912@users.noreply.github.com>
2023-10-08 14:47:59 +0530

    Update token.h

--- token.h
+++ token.h
@@ -3,28 +3,28 @@
 #include <time.h>
 #include <stdio.h>
 #include <string.h>
-using namespace std;
 #pragma once
 class token
 {private:
     static int no_of_tokens;
-    char applied_tokens[20][50];
+    std::vector<std::string> applied_tokens(20);
 public:
     void apply_token();
     int ret_token();
 };
 int token::no_of_tokens=0;
-int token::ret_token()
+int token::ret_token() const
 {   return no_of_tokens;
 }
 void token::apply_token()
-{   char vfood[12][30]={"Kadai paneer","chilli gobi","pav bhaji","gobi manchurian","vada pav","lasagne","pasta","veg biryani","aloo makan","aloo tiki"};
-    char nvfood[12][30]={"Butter chicken","chicken 65","biryani","chicken tikka masala","omellete","kolidhi chicken","fish cury","chicken tikka masala","chicken tikka"};
+{   std::vector<std::string> vfood={"Kadai paneer","chilli gobi","pav bhaji","gobi manchurian","vada pav","lasagne","pasta","veg biryani","aloo makan","aloo tiki"};
+    std::vector<std::string> nvfood[12][30]={"Butter chicken","chicken 65","biryani","chicken tikka masala","omellete","kolidhi chicken","fish cury","chicken tikka masala","chicken tikka"};
     cout<<"\n Types of token 1.veg 2.non veg:";
     int ch2;
     cin>>ch2;
     if(ch2==1)
-    {   srand(time(0));
+    {   
+        srand(time(0));
         int d=rand()%10;
         cout<<"\n Today s token is :" <<vfood[d]<<"\n Do you want to apply(y/n)";
         char ch3;
@@ -35,13 +35,15 @@ void token::apply_token()
         }
     }
     else if(ch2==2)
-    {   srand(time(0));
+    {   
+        srand(time(0));
         int d=rand()%10;
         cout<<"\n Today s token is :" <<nvfood[d]<<"\n Do you want to apply(y/n)";
         char ch3;
         cin>>ch3;
         if(ch3=='y'||ch3=='Y')
-        {   no_of_tokens++;
+        {   
+            no_of_tokens++;
             strcpy(applied_tokens[no_of_tokens-1],nvfood[d]);
         }
     }
