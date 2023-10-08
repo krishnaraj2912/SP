1b058eaa304b4e66d4449bd511ef26b9d867a9b5
Krishna Raj <62538406+krishnaraj2912@users.noreply.github.com>
2023-10-08 14:06:05 +0530

    Update bill.h

--- bill.h
+++ bill.h
@@ -1,13 +1,14 @@
 #include <iostream>
 #pragma once
-using namespace std;
+
 class bill
 {
     float total_bill;
     float cash;
 
 public:
-    bill() : cash(50000) {
+    bill() : {
+        cash = 50000;
     }
     void update_balance(float balance)
     {   
@@ -15,11 +16,11 @@ public:
     }
     void calc_bill(int no_of_tokens)
     {
-        total_bill=3000+(50*no_of_tokens);
-        cash-=total_bill;
+        total_bill = 3000+(50*no_of_tokens);
+        cash -= total_bill;
         //(3000 basic amount)
     }
-    void print_bill const(string name, int no_of_tokens)
+    void print_bill const(std::string name, int no_of_tokens)
     {   
         cout<<"\n Name:"<<name<<"\n No of tokens registered :"<<no_of_tokens;
         cout<<"\n The bill is :"<<total_bill;
